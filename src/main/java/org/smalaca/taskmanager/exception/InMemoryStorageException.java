7be047b65e68133b7a1d2e9bc2f7e64b1d2dd723
smalaca <ab13.krakow@gmail.com>
2016-11-16 14:13:12 +0100

    Remove useless InMemoryStorageException

--- src/main/java/org/smalaca/taskmanager/exception/InMemoryStorageException.java
+++
@@ -1,7 +0,0 @@
-package org.smalaca.taskmanager.exception;
-
-public class InMemoryStorageException extends RuntimeException {
-    public InMemoryStorageException(String message) {
-        super(message);
-    }
-}
