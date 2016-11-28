030962db55703769c26644fcaf8e1b286c017165
smalaca <ab13.krakow@gmail.com>
2016-11-28 19:00:52 +0100

    Add constructors to the ToDoItem[...]EventsTrgigger

--- src/main/java/org/smalaca/taskmanager/trigger/ToDoItemInProgressEventsTrigger.java
+++ src/main/java/org/smalaca/taskmanager/trigger/ToDoItemInProgressEventsTrigger.java
@@ -1,8 +1,17 @@
 package org.smalaca.taskmanager.trigger;
 
 import org.smalaca.taskmanager.domain.ToDoItem;
+import org.smalaca.taskmanager.service.CommunicationService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class ToDoItemInProgressEventsTrigger implements CommunicationEventTrigger {
+    private final CommunicationService communicationService;
+
+    @Autowired
+    public ToDoItemInProgressEventsTrigger(CommunicationService communicationService) {
+        this.communicationService = communicationService;
+    }
+
     @Override
     public boolean isApplicableFor(ToDoItem toDoItem) {
         return false;
