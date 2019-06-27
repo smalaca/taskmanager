0c69b42292a8d8952e2a682c0371c51fd19eb8af
smalaca <ab13.krakow@gmail.com>
2019-06-27 07:59:23 +0200

    Add ProjectStatus

--- src/main/java/org/smalaca/taskmanager/domain/Project.java
+++ src/main/java/org/smalaca/taskmanager/domain/Project.java
@@ -3,7 +3,10 @@ package org.smalaca.taskmanager.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.smalaca.taskmanager.domain.ProjectStatus.DEFINED;
+
 public class Project {
+    private ProjectStatus projectStatus = DEFINED;
     private String id;
     private String name;
     private ProductOwner productOwner;
@@ -12,6 +15,10 @@ public class Project {
     public Project(ProductOwner productOwner, String name) {
         this.productOwner = productOwner;
         this.name = name;
+    }
+
+    public void change(ProjectStatus projectStatus) {
+        this.projectStatus = projectStatus;
     }
 
     public ProductOwner getProductOwner() {
