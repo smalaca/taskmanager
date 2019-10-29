66fa1d0ee7a83662a0427c78eae5c5047b4967c7
smalaca <ab13.krakow@gmail.com>
2019-10-29 15:23:09 +0100

    Start using state pattern and ToDoItemState

--- src/main/java/org/smalaca/taskmanager/processor/ToDoItemProcessor.java
+++ src/main/java/org/smalaca/taskmanager/processor/ToDoItemProcessor.java
@@ -1,16 +1,8 @@
 package org.smalaca.taskmanager.processor;
 
-import org.smalaca.taskmanager.domain.Epic;
-import org.smalaca.taskmanager.domain.Story;
-import org.smalaca.taskmanager.domain.Task;
-import org.smalaca.taskmanager.domain.ToDoItem;
-import org.smalaca.taskmanager.event.EpicReadyToPrioritize;
+import com.google.common.collect.ImmutableMap;
+import org.smalaca.taskmanager.domain.*;
 import org.smalaca.taskmanager.event.EventPublisher;
-import org.smalaca.taskmanager.event.StoryApprovedEvent;
-import org.smalaca.taskmanager.event.StoryDoneEvent;
-import org.smalaca.taskmanager.event.TaskApprovedEvent;
-import org.smalaca.taskmanager.event.ToDoItemReleasedEvent;
-import org.smalaca.taskmanager.exception.UnsupportedToDoItemType;
 import org.smalaca.taskmanager.repository.StoryRepository;
 import org.smalaca.taskmanager.service.CommunicationService;
 import org.smalaca.taskmanager.service.ProjectBacklogService;
@@ -18,41 +10,29 @@ import org.smalaca.taskmanager.service.SprintBacklogService;
 import org.smalaca.taskmanager.service.StoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.smalaca.taskmanager.domain.Status.DONE;
+import java.util.Map;
+
+import static org.smalaca.taskmanager.domain.Status.*;
 
 public class ToDoItemProcessor {
-
     private final ToDoItemProcessorFacade toDoItemProcessorFacade;
+    private final Map<Status, ToDoItemState> states;
 
     @Autowired
     public ToDoItemProcessor(
             StoryRepository storyRepository, StoryService storyService, EventPublisher eventPublisher,
             ProjectBacklogService projectBacklogService, CommunicationService communicationService, SprintBacklogService sprintBacklogService) {
-
         toDoItemProcessorFacade = new ToDoItemProcessorFacade(storyRepository, storyService, eventPublisher, projectBacklogService, communicationService, sprintBacklogService);
+        states = ImmutableMap.of(
+                DEFINED, new ToDoItemDefined(toDoItemProcessorFacade),
+                IN_PROGRESS, new ToDoItemInProgress(toDoItemProcessorFacade),
+                DONE, new ToDoItemDone(toDoItemProcessorFacade),
+                APPROVED, new ToDoItemApproved(toDoItemProcessorFacade),
+                RELEASED, new ToDoItemReleased(toDoItemProcessorFacade)
+        );
     }
 
     public void processFor(ToDoItem toDoItem) {
-        switch(toDoItem.getStatus()) {
-            case DEFINED:
-                new ToDoItemDefined(toDoItemProcessorFacade).process(toDoItem);
-                break;
-
-            case IN_PROGRESS:
-                new ToDoItemInProgress(toDoItemProcessorFacade).process(toDoItem);
-                break;
-
-            case DONE:
-                new ToDoItemDone(toDoItemProcessorFacade).process(toDoItem);
-                break;
-
-            case APPROVED:
-                new ToDoItemApproved(toDoItemProcessorFacade).process(toDoItem);
-                break;
-
-            case RELEASED:
-                new ToDoItemReleased(toDoItemProcessorFacade).process(toDoItem);
-                break;
-        }
+        states.get(toDoItem.getStatus()).process(toDoItem);
     }
 }
